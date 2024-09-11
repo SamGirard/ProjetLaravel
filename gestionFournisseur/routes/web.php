461fadceaa7ff2f5fd57d24e7f9b64ab70c8da7d
ivanlandry <58294595+ivanlandry@users.noreply.github.com>
2024-09-11 05:12:02 -0400

    formulaire de creation de fournisseur

--- gestionFournisseur/routes/web.php
+++ gestionFournisseur/routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\FournisseurController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/ajouter',[FournisseurController::class,'create_identification']);
 Route::get('/ajouter_service',[FournisseurController::class,'create_service'])->name('create_service');
