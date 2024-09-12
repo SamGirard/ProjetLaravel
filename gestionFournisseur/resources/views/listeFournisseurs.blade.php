6a6e673cc26cb0339202ce9b36f6c4de75e5c287
tijaycegep <143433393+tijaycegep@users.noreply.github.com>
2024-09-12 11:19:09 -0400

    b

--- gestionFournisseur/resources/views/listeFournisseurs.blade.php
+++ gestionFournisseur/resources/views/listeFournisseurs.blade.php
@@ -1,8 +1,11 @@
-<script src="https://cdn.tailwindcss.com"></script>
-<script src="https://unpkg.com/alpinejs" defer></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <script src="https://unpkg.com/alpinejs" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
 <div x-data="{ slideOverOpen: false }" class="relative z-50 w-auto h-auto">
     <button 
         @click="slideOverOpen = true" 
