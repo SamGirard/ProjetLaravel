0eafffcfa5322d3a7eed53d5e10884a39a360dc3
SamGirard <sgirard2004@icloud.com>
2024-09-26 07:59:10 -0400

    js pour le role réajouter

--- gestionFournisseur/resources/views/layouts/app.blade.php
+++ gestionFournisseur/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
     <body>
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
@@ -14,7 +15,7 @@
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Trois-Rivières</span>
                 </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <form action="{{ route('logout') }}" method="POST">
+                <form action="{{ route('logout') }}" method="post">
                     @csrf
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Déconnexion</button>
                 </form>
